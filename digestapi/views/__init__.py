b9d6f378cdc496488f25c0a00540213f0d20e88a
Chad Cote <chadcote58@gmail.com>
2024-07-26 14:46:46 -0400

    Created seed data for categories created views/categories and fixtures/categories

--- digestapi/views/__init__.py
+++ digestapi/views/__init__.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from digestapi.views.users import UserViewSet
+from digestapi.views.categories import CategoryViewSet
 
 router = DefaultRouter(trailing_slash=False)
 
